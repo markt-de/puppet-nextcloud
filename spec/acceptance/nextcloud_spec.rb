e8b153819c976a2c7c42ebad42339da2055d348e
Frank Wall <fw@moov.de>
2021-03-02 15:57:11 +0100

    flip nobody/nogroup

--- spec/acceptance/nextcloud_spec.rb
+++ spec/acceptance/nextcloud_spec.rb
@@ -8,8 +8,8 @@ describe 'nextcloud class' do
       class { 'nextcloud':
         admin_password => 'suPeRseCreT',
         db_password    => 'secRetPasSwOrd',
-        system_group   => 'nobody',
-        system_user    => 'nogroup',
+        system_group   => 'nogroup',
+        system_user    => 'nobody',
         version        => '21.0.0',
       }
       EOS
